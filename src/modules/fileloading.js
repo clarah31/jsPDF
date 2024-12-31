2e08aa37f9328fc399afbff260d3cb034fe5ab54
clarah31 <muc6104@gmail.com>
2024-12-31 13:28:12 +0100

    ttf binary

--- src/modules/fileloading.js
+++ src/modules/fileloading.js
@@ -94,28 +94,28 @@ import { jsPDF } from "../jspdf.js";
   function nodeReadFile(url, sync, callback) {
     sync = sync === false ? false : true;
     var result = undefined;
+console.log("nodeReadFile not implement")
+    // var fs = require("fs");
+    // var path = require("path");
 
-    var fs = require("fs");
-    var path = require("path");
-
-    url = path.resolve(url);
-    if (sync) {
-      try {
-        result = fs.readFileSync(url, { encoding: "latin1" });
-      } catch (e) {
-        return undefined;
-      }
-    } else {
-      fs.readFile(url, { encoding: "latin1" }, function(err, data) {
-        if (!callback) {
-          return;
-        }
-        if (err) {
-          callback(undefined);
-        }
-        callback(data);
-      });
-    }
+    // url = path.resolve(url);
+    // if (sync) {
+    //   try {
+    //     result = fs.readFileSync(url, { encoding: "latin1" });
+    //   } catch (e) {
+    //     return undefined;
+    //   }
+    // } else {
+    //   fs.readFile(url, { encoding: "latin1" }, function(err, data) {
+    //     if (!callback) {
+    //       return;
+    //     }
+    //     if (err) {
+    //       callback(undefined);
+    //     }
+    //     callback(data);
+    //   });
+    // }
 
     return result;
   }
--- src/modules/ttfsupport.js
+++ src/modules/ttfsupport.js
@@ -25,11 +25,12 @@ import { atob } from "../libs/AtobBtoa.js";
 
   var addFont = function(font, file) {
     // eslint-disable-next-line no-control-regex
-    if (/^\x00\x01\x00\x00/.test(file)) {
-      file = binaryStringToUint8Array(file);
-    } else {
-      file = binaryStringToUint8Array(atob(file));
-    }
+    // if (/^\x00\x01\x00\x00/.test(file)) {
+    //   file = binaryStringToUint8Array(file);
+    // } else {
+    //   file = binaryStringToUint8Array(atob(file));
+    // }
+    file =new Uint8Array(file)
     font.metadata = jsPDF.API.TTFFont.open(file);
     font.metadata.Unicode = font.metadata.Unicode || {
       encoding: {},
@@ -54,13 +55,13 @@ import { atob } from "../libs/AtobBtoa.js";
         } else {
           file = instance.getFileFromVFS(font.postScriptName);
         }
-        if (typeof file !== "string") {
-          throw new Error(
-            "Font is not stored as string-data in vFS, import fonts or remove declaration doc.addFont('" +
-              font.postScriptName +
-              "')."
-          );
-        }
+        // if (typeof file !== "string") {
+        //   throw new Error(
+        //     "Font is not stored as string-data in vFS, import fonts or remove declaration doc.addFont('" +
+        //       font.postScriptName +
+        //       "')."
+        //   );
+        // }
         addFont(font, file);
       } else {
         throw new Error(
